8fa7d2c1f3cc898811821076415344d90132b4c6
Anirudha <anirudhaj424@gmail.com>
2024-11-11 12:37:36 -0800

    correction

--- src/Components/Img.jsx
+++ src/Components/Img.jsx
@@ -4,6 +4,9 @@ const Image = styled.img`
   height: 4rem;
   width: 4rem;
   border-radius: 50%;
+  @media (min-width: 768px) {
+    align-items: center;
+  }
 `;
 
 export default function Img({ src, alt }) {
